d1eca9e3f732b14f6b4df64114cb960bce722b58
absmohamed <cabdullahidaud@Hotmail.com>
2020-01-12 18:53:01 +1000

    Added nodemon and moved private information like the MongoURI into dev.js and then updated the gitignore

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://abs:abcd1234@cloud-fitness-n9lwh.mongodb.net/test?retryWrites=true&w=majority', 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require('./config/key');
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI, 
     {useUnifiedTopology: true, useNewUrlParser: true}).then(() => console.log("MongoDB connected"))
                             .catch(err => console.error(err));
 
 
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.json({"hello ~": "Hi ~~ "})
+})
+
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body)
+
+    user.save((err, userData) => {
+        if(err) return res.json ({ success: false, err})
+    })
+    return res.status(200).json({
+        success: true
+    })
+})
 
 app.get('/', (req,res) => {
     res.send('Hello World')

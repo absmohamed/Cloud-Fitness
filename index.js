6b67fcf652a77f4082558bacbd847e098d82569b
absmohamed <cabdullahidaud@Hotmail.com>
2020-01-13 14:42:16 +1000

    Comitting work

--- index.js
+++ index.js
@@ -82,4 +82,8 @@ app.get('/api/user/logout', auth, (req,res) => {
     })
 });
 
-app.listen(5000);
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server running at ${port}`)
+});

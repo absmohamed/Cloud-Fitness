70fe70fb7797095d52cf5ddd7e336784d1dc313e
absmohamed <cabdullahidaud@Hotmail.com>
2020-01-15 14:44:03 +1000

    Refactored the routes into a routes folder for User Authentication, added passportt-jsonwebtoken srategy, updated models/user and updated index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const users = require('./routes/api/users');
+const passport = require('passport');
+const app = express();
 
-const config = require('./config/key');
-const { User } = require('./models/user');
-const { auth } = require('./middleware/auth');
-
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/key').mongoURI
+const db = require('./config/keys').mongoURI
 
 // Connect to MongoDB
 mongoose
@@ -19,75 +19,14 @@ mongoose
     .catch(err => console.error(err));
 
 
-app.use(bodyParser.urlencoded({ extended : true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
+// Passport Middleware
+app.use(passport.initialize());
 
+// Passport Config
+require('./config/passport')(passport);
+// Use Routes
+app.use('/api/users', users);
 
-app.get("/api/user/auth", auth, (req, res) => {
-    res.status(200).json({
-        _id:req._id,
-        isAuth: true,
-        email: req.user.email,
-        name: req.user.name,
-        lastname: req.user.lastname,
-        role: req.user.role
-    })
-})
-
-app.post('/api/users/register', (req,res) => {
-        const user = new User(req.body)
-
-        user.save((err, doc) => {
-        if(err) return res.json ({ success: false, err});
-        res.status(200).json({
-            success: true,
-            userData: doc
-        })
-    })
-})
-
-app.post('/api/user/login', (req, res) => {
-    // Find the email
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if(!user)
-        return res.json({
-            loginSucess: false,
-            message: "Authentication failed, email not found"
-        });
-
-     // Compare the password
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch) {
-                return res.json({ 
-                    loginSuccess: false, 
-                    message: "Password is incorrect"
-                })
-            }
-        })
-
-     // generateToken
-        user.generateToken((err, user) => {
-            if(err) return res.status(400).send(err);
-            res.cookie("x_auth", user.token)
-                .status(200)
-                .json({
-                    loginSuccess: true
-                })
-        })
-
-    })
-
-})
-
-app.get('/api/user/logout', auth, (req,res) => {
-    User.findOneAndUpdate({_id: req.user._id}, { token: ""}, (err, doc) => {
-        if(err) return res.json({success: false, err })
-        return res.status(200).send({
-            success: true
-        })
-    })
-});
 
 const port = process.env.PORT || 5000
 

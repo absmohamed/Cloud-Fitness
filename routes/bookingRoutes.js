39ad42429e7d1c7ad61b1db0f805cf3f96d53971
vicnatacu <vicki@mvbmb.com>
2020-01-15 15:19:06 +1000

    changes to bookingRoutes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
-//const Booking = require("../models/booking")
+const Booking = require("../models/booking")
 
-// router.get("/", ((req, res)=> {
-//     //Booking.find(_id, req.user._id).then((bookings) => {
-//         res.statusCode(200);
-//         res.send(bookings);
-//     }).catch((err) => {
+router.get("/", ((req, res)=> {
+    Booking.find().then((bookings) => {
+        res.statusCode(200);
+        res.send(bookings);
+    }).catch((err) => {
+        
+    })
+}))
 
-//     })
-// )
+router.post("/", (req, res) => {
+    createBooking = async (bookingInfo) => {
+        //call to server to add new booking
+        //will return the new booking
+        const newBooking = {
+            service: bookingInfo.service,
+            level: bookingInfo.level,
+            datetime: bookingInfo.datetime,
+            duration: bookingInfo.duration,
+            
+
+
+
+        }
+    }
+})
